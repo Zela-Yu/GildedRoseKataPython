e9c232979febd2b955e0919251be243f0c57e584
Zhiliang Yu <zhiliangyu@bbq.local>
2025-02-12 22:14:53 -0800

    refact with strategy pattern

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -2,7 +2,7 @@
 import unittest
 
 from gilded_rose import Item, GildedRose
-from gilded_rose import Item, NormalItem
+from gilded_rose import Item, NormalItem, AgedBrie, BackstagePass, Sulfuras
 
 
 class GildedRoseTest(unittest.TestCase):
@@ -29,6 +29,62 @@ class GildedRoseTest(unittest.TestCase):
         normal.update()
         self.assertEqual(item.sell_in, 9)
         self.assertEqual(item.quality, 19)
+
+    def test_normal_item_expired(self):
+        item = Item("Normal Item", 0, 10)
+        normal = NormalItem(item)
+        normal.update()
+        self.assertEqual(item.sell_in, -1)
+        self.assertEqual(item.quality, 8) 
+
+    def test_aged_brie(self):
+        item = Item("Aged Brie", 5, 10)
+        brie = AgedBrie(item)
+        brie.update()
+        self.assertEqual(item.sell_in, 4)
+        self.assertEqual(item.quality, 11)
+
+    def test_aged_brie_expired(self):
+        item = Item("Aged Brie", 0, 10)
+        brie = AgedBrie(item)
+        brie.update()
+        self.assertEqual(item.sell_in, -1)
+        self.assertEqual(item.quality, 12)
+
+    def test_backstage_pass_high_sell_in(self):
+        item = Item("Backstage passes to a TAFKAL80ETC concert", 15, 20)
+        pass_item = BackstagePass(item)
+        pass_item.update()
+        self.assertEqual(item.sell_in, 14)
+        self.assertEqual(item.quality, 21)
+
+    def test_backstage_pass_mid_sell_in(self):
+        item = Item("Backstage passes to a TAFKAL80ETC concert", 10, 20)
+        pass_item = BackstagePass(item)
+        pass_item.update()
+        self.assertEqual(item.sell_in, 9)
+        self.assertEqual(item.quality, 22)
+
+    def test_backstage_pass_low_sell_in(self):
+        item = Item("Backstage passes to a TAFKAL80ETC concert", 5, 20)
+        pass_item = BackstagePass(item)
+        pass_item.update()
+        self.assertEqual(item.sell_in, 4)
+        self.assertEqual(item.quality, 23)
+
+    def test_backstage_pass_expired(self):
+        item = Item("Backstage passes to a TAFKAL80ETC concert", 0, 20)
+        pass_item = BackstagePass(item)
+        pass_item.update()
+        self.assertEqual(item.sell_in, -1)
+        self.assertEqual(item.quality, 0)
+
+    def test_sulfuras(self):
+        item = Item("Sulfuras, Hand of Ragnaros", 5, 80)
+        sulfuras = Sulfuras(item)
+        sulfuras.update()
+        self.assertEqual(item.sell_in, 5)
+        self.assertEqual(item.quality, 80) 
 
 if __name__ == '__main__':
     unittest.main()
